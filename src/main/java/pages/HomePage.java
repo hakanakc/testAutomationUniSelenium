cc4ac7e1465ba2d871e33a7aaeb003217d38a072
hakanakc <akkayahakan3448@gmail.com>
2021-06-23 15:49:12 +0100

    Initial commit

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,40 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    private WebDriver driver;
+
+    // asagi satiri silmeyi dene
+ //   private By formAuthenticationLink = By.linkText("Form Authentication");
+
+   public HomePage(WebDriver driver){
+  this.driver =driver;
+
+
+   }
+
+   public LoginPage  clickFormAuthentication(){
+  clickLink("Form Authentication");
+  return new LoginPage (driver);
+
+   }
+
+   public DropdownPage clickDropDown(){
+
+       clickLink("Dropdown");
+       return new DropdownPage(driver);
+   }
+
+   public HoversPage clickHovers() {
+     clickLink("Hovers");
+     return new HoversPage(driver);
+   }
+
+   private void clickLink(String linkText) {
+    driver.findElement(By.linkText(linkText)).click();
+
+}
+}
